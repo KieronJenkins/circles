2c71dfba47b93ad8703c8915b02813eceb450aa5
Kieron Jenkins <KOJ1@hotmail.co.uk>
2018-04-15 13:59:54 +0100

    Commit - 002
    Start of the main game.

---
+++ Circles/Assets/Scripts/CircleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleScript : MonoBehaviour {
+
+	private float _rotateSpeed = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Rotate (0f, 0f, _rotateSpeed + Time.deltaTime);
+
+	}
+}
---
+++ Circles/Assets/Scripts/PlayerBounce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBounce : MonoBehaviour {
+
+	[SerializeField]
+	private float _playerBounce = 475f; 
+	[SerializeField]
+	private Rigidbody2D _rb;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump") || Input.GetMouseButtonDown (0)) 
+		{
+			_rb.velocity = Vector2.up * _playerBounce;
+		}
+	}
+}
